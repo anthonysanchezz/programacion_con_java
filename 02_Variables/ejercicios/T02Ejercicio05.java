e96865f7c171de6de8519720c7fed4923f56acba
Antonio Sánchez Caparrós <142712862+anthonysanchezz@users.noreply.github.com>
2023-11-08 20:28:42 +0100

    Arreglo en conversor de pesetas a euros

--- 02_Variables/ejercicios/T02Ejercicio05.java
+++ 02_Variables/ejercicios/T02Ejercicio05.java
@@ -8,8 +8,8 @@
  */
 public class T02Ejercicio05 {
   public static void main(String[] args) {
-    int pesetas = 998;
-    double euros = pesetas / 166.386;
+    int peseta = 998;
+    double euros = peseta / 166.386;
 
     System.out.printf("%.2f", euros);
   }
