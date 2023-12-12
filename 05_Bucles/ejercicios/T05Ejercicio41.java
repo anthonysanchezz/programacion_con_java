a1ee0fd11b99bb094721a7658e72b07d05b351ce
Antonio Sánchez Caparrós <142712862+anthonysanchezz@users.noreply.github.com>
2023-12-12 11:58:27 +0100

    T05Ejercicio41.java

--- 05_Bucles/ejercicios/T05Ejercicio41.java
+++ 05_Bucles/ejercicios/T05Ejercicio41.java
@@ -1,6 +1,3 @@
-> .[!WARNING]
-> No funciona
-
 /**
  * Escribe un programa que diga cuántos dígitos pares y cuántos dígitos impares
  * hay dentro de un número. Se recomienda usar long en lugar de int ya que el
